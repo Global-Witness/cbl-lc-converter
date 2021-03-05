19ee1157a0b9bd2dc99fb3ccc0a28e6aa493c8d5
Louis Goddard <louisgoddard@gmail.com>
2021-03-05 18:30:27 +0000

    Initial upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# cbl-lc-converter
-Script to extract tabular data from Central Bank of Libya LC disclosures
+# CBL LC converter
+
+This script is designed to extract data tables from PDF disclosures of Letter of Credit applications made by the Central Bank of Libya. An example of a disclosure can be found [on the CBL Facebook page](https://www.facebook.com/CentralBankofLibya/posts/5059776977426419).
+
+Run the script from within this directory using the command `Rscript converter.R`, or by opening it in an IDE like RStudio. Before running it, update the file `.Renviron` to set the relevant environment variables, as described below.
+
+## Environment variables
+
+| Variable name | Description |
+| ------------- | ----------- |
+| `PDF_PATH` | The location of the PDF file to extract data from. |
+| `CSV_PATH` | The desired location of the output file in CSV format. |
+| `MARGIN_[1–6]` | The x coordinate of the left-hand margin of each column (see [this Stack Overflow answer](https://stackoverflow.com/a/2592991) for some sugestions about how to calculate x, y coordinates from PDFs). |
+| `COLUMN_NAME_[1–6]` | The desired name of each columns in the output file. |
---
+++ convert.R
@@ -0,0 +1,58 @@
+library(dplyr)
+library(readr)
+library(purrr)
+library(tidyr)
+library(stringr)
+library(pdftools)
+
+data <- pdf_data(Sys.getenv("PDF_PATH"))
+
+LEFT_MARGINS <- as.numeric(c(
+  Sys.getenv("MARGIN_1"),
+  Sys.getenv("MARGIN_2"),
+  Sys.getenv("MARGIN_3"),
+  Sys.getenv("MARGIN_4"),
+  Sys.getenv("MARGIN_5")))
+
+COLUMN_NAMES <- c(
+  Sys.getenv("COLUMN_NAME_1"),
+  Sys.getenv("COLUMN_NAME_2"),
+  Sys.getenv("COLUMN_NAME_3"),
+  Sys.getenv("COLUMN_NAME_4"),
+  Sys.getenv("COLUMN_NAME_5"),
+  Sys.getenv("COLUMN_NAME_6"))
+
+extract_table <- function(page) {
+  page %>%
+    arrange(y, x) %>%
+    mutate(
+      new_row_flag = case_when(
+        y - lag(y) > 10 ~ 1,
+        TRUE ~ 0),
+      row_id = cumsum(new_row_flag) + 1,
+      cell_id = case_when(
+        x <= LEFT_MARGINS[1] ~ COLUMN_NAMES[1],
+        between(x, LEFT_MARGINS[1], LEFT_MARGINS[2] - 1) ~ COLUMN_NAMES[2],
+        between(x, LEFT_MARGINS[2], LEFT_MARGINS[3] - 1) ~ COLUMN_NAMES[3],
+        between(x, LEFT_MARGINS[3], LEFT_MARGINS[4] - 1) ~ COLUMN_NAMES[4],
+        between(x, LEFT_MARGINS[4], LEFT_MARGINS[5] - 1) ~ COLUMN_NAMES[5],
+        x >= LEFT_MARGINS[5] ~ COLUMN_NAMES[6])) %>%
+    select(row_id, cell_id, text) %>%
+    rowwise() %>%
+    mutate(text = case_when(
+      !str_detect(str_to_upper(text), "[A-Z0-9]") ~ str_c(rev(str_split(text, "")[[1]]), collapse = ""),
+      TRUE ~ text)) %>%
+    arrange(row_id, cell_id) %>%
+    group_by(row_id, cell_id) %>%
+    summarise(text = case_when(
+      !str_detect(str_to_upper(str_c(text, collapse = " ")), "[A-Z0-9]") ~ str_c(rev(text), collapse = " "),
+      TRUE ~ str_c(text, collapse = " ")),
+      .groups = "drop") %>%
+    pivot_wider(id_cols = row_id, names_from = cell_id, values_from = text) %>%
+    filter(!is.na(COLUMN_NAMES[1])) %>%
+    select(row_id, COLUMN_NAMES)
+}
+
+data %>%
+  map_dfr(extract_table) %>%
+  write_csv(Sys.getenv("CSV_PATH"))
